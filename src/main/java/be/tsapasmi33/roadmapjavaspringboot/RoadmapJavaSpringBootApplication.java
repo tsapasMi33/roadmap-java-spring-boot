98f1bb9383e22499c769126c7488534c0d3ffab5
tsapasMi33 <tsapas_mi@hotmail.com>
2024-10-12 17:56:50 +0200

    After framework installation

---
+++ src/main/java/be/tsapasmi33/roadmapjavaspringboot/RoadmapJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package be.tsapasmi33.roadmapjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmapJavaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadmapJavaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/tsapasmi33/roadmapjavaspringboot/RoadmapJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package be.tsapasmi33.roadmapjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapJavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
